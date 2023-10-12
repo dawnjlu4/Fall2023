5bb5748a4f323316ec4c80326a4750d6a5217b53
dawnjlu4 <djl573@nyu.edu>
2023-10-12 04:03:26 +0000

    #dawnjlu4

--- xll_fre/fre_bsm.h
+++ xll_fre/fre_bsm.h
@@ -14,9 +14,9 @@ namespace fre::bsm {
 		//                  --bond--|-------stock--------  ------option------
 		inline double value(double r, double S0, double σ, double k, double t)
 		{
-			double R = 0; //!!! implement this
-			double f = 0; //!!! implement this
-			double s = 0; //!!! implement this
+			double R = std::exp(r * t);
+			double f = R * S0;
+			double s = σ * std::sqrt(t);
 
 			return fre::black::put::value(f, s, k)/R;
 		}
